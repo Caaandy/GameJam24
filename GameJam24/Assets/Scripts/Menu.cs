2bfe9941ab998b0fef713c41286857083841c92c
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2024-11-16 14:04:52 +0100

    basic menu
    also added text mesh pro stuff

---
+++ GameJam24/Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu: MonoBehaviour 
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
+
