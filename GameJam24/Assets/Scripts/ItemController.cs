f0bf4068946fdf768338fc2a28c3671c8785bb12
Runebreaker <gino.schulze-skyfire@web.de>
2024-11-17 15:42:47 +0100

    Add item bobbing

--- GameJam24/Assets/Scripts/ItemController.cs
+++ GameJam24/Assets/Scripts/ItemController.cs
@@ -1,20 +1,23 @@
-using System;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
     public Item item;
+    public float bobbingIntensity = 0.2f;
+    public float bobbingSpeed = 1.0f;
+    private Vector3 fixedPosition;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        fixedPosition = transform.position;
         GetComponent<SpriteRenderer>().sprite = item.sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = fixedPosition + Mathf.Cos(Time.time * bobbingSpeed) * bobbingIntensity * Vector3.up;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
