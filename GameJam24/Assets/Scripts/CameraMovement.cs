7b800adaee695f07127f9fa401d146f954d7d53d
Runebreaker <gino.schulze-skyfire@web.de>
2024-11-17 13:20:23 +0100

    Separated Responsibilities in camera movement script

--- GameJam24/Assets/Scripts/CameraMovement.cs
+++ GameJam24/Assets/Scripts/CameraMovement.cs
@@ -1,16 +1,17 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     #region References
     public GameObject player;
-    public GameObject wall;
+    public WallPlacement wallPlacement;
     public float smoothing = 1.0f;
-    private GameObject wallInstance;
-    public float wallWidth = 1.0f;
     public float camHeightOffset = 0.0f;
     private Vector3 playerFurtherstPosition;
     private Vector3 camVelocity = Vector2.zero;
+    private Camera mainCamera;
 
     //is this the first time we call start?
     private bool detlef = true;
@@ -20,33 +21,27 @@ public class CameraMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!mainCamera)
+        {
+            mainCamera = Camera.main;
+        }
         if(!detlef)
         {
             Vector3 cameraDistance = playerFurtherstPosition - transform.position;
             transform.position = player.transform.position - cameraDistance;
             playerFurtherstPosition = player.transform.position;
         }
-        if (wall != null)
-        {
-            var wallSize = new Vector3(wallWidth, Screen.height, 0); 
-            wallInstance = Instantiate(wall);
-            wallInstance.GetComponent<BoxCollider2D>().size = wallSize;
-            wallInstance.transform.position = CalculateWallPosition();
-            wallInstance.transform.localScale = new Vector3(wallWidth, Screen.height, 1.0f);
-        }
         if (player != null && detlef)
         {
             playerFurtherstPosition = player.transform.position;
             transform.position = CalculateTargetPosition(camHeightOffset);
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position, CalculateTargetPosition(camHeightOffset), ref camVelocity, smoothing); // Smoothly move the camera to the player's position
-        wallInstance.transform.position = CalculateWallPosition();
     }
 
     void FixedUpdate()
@@ -70,17 +65,10 @@ public class CameraMovement : MonoBehaviour
         return new Vector3(playerFurtherstPosition.x, playerFurtherstPosition.y + offset, -10.0f);
     }
 
-    private Vector3 CalculateWallPosition()
-    {
-        var wallScreenPosition = new Vector3(- wallWidth / 2.0f, Screen.height / 2.0f, player.transform.position.z);
-        var rawPosition = Camera.main.ScreenToWorldPoint(wallScreenPosition);
-        return new Vector3(rawPosition.x, rawPosition.y, player.transform.position.z);
-    }
-
     public void Reset()
     {
         detlef = false;
-        GameObject.Destroy(wallInstance);
+        wallPlacement.Reset();
         Start(); 
     }
 }
---
+++ GameJam24/Assets/Scripts/WallPlacement.cs
@@ -0,0 +1,61 @@
+using System;
+using Unity.Jobs;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WallPlacement : MonoBehaviour
+{
+    #region References
+    public GameObject wall;
+    public Texture2D wallTexture;
+    private GameObject wallInstance;
+    public float wallWidth = 1.0f;
+    private Vector2 wallSize;
+    private Camera mainCamera;
+    #endregion
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!mainCamera)
+        {
+            mainCamera = Camera.main;
+        }
+        if (wall != null)
+        {
+            wallSize = new Vector2(wallWidth * 2, 20.0f);
+            wallInstance = Instantiate(wall);
+            wallInstance.transform.parent = mainCamera.transform;
+            var collider = wallInstance.GetComponent<BoxCollider2D>();
+            collider.size = new Vector2(wallSize.x / 4.0f, wallSize.y);
+            collider.offset = new Vector2(collider.size.x / 2.0f, 0.0f);
+            wallInstance.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            wallInstance.transform.position = CalculateWallPosition();
+            var renderer = wallInstance.GetComponent<SpriteRenderer>();
+            renderer.drawMode = SpriteDrawMode.Tiled;
+            renderer.size = wallSize;
+            renderer.sprite = Sprite.Create(wallTexture, new Rect(0.0f, 0.0f, wallTexture.width, wallTexture.height), new Vector2(0.0f, 0.5f), 32);
+            renderer.sortingOrder = 1;
+        }
+        wallInstance.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    void Update() {
+        // For some reason it has a slight parallax effect, but we take it
+        wallInstance.transform.position = new Vector3(wallInstance.transform.position.x, - mainCamera.transform.position.y, wallInstance.transform.position.z);
+    }
+
+    public Vector3 CalculateWallPosition()
+    {
+        var wallScreenPosition = new Vector3(0, Screen.height / 2.0f, 0);
+        var rawPosition = mainCamera.ScreenToWorldPoint(wallScreenPosition);
+        return new Vector3(rawPosition.x, 0, 0);
+    }
+
+    public void Reset()
+    {
+        Destroy(wallInstance);
+        Start();
+    }
+}
